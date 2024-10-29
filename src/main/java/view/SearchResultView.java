49a0a739766f9d49a20dd0ea6e555ba4ac7e7b91
cormacyounguoft <cormac.young@mail.utoronto.ca>
2024-10-29 14:20:26 -0400

    Improved SearchResultView

--- src/main/java/view/SearchResultView.java
+++ src/main/java/view/SearchResultView.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import entity.Movie;
 import interface_adapter.search_result.SearchResultController;
 import interface_adapter.search_result.SearchResultState;
 import interface_adapter.search_result.SearchResultViewModel;
@@ -24,7 +25,15 @@ public class SearchResultView extends JPanel implements ActionListener, Property
     private final SearchResultViewModel searchResultViewModel;
 
     private final JButton toHome;
-    private final JLabel movie;
+    private final JPanel movie;
+    private final JLabel movieTitle;
+    private final JLabel movieReleaseDate;
+    private final JLabel movieDescription;
+    private final JLabel movieRottenTomatoes;
+    private final JLabel movieGenre;
+    private final JLabel movieActors;
+    private final JLabel movieDirector;
+
     private SearchResultController searchResultController;
 
     public SearchResultView(SearchResultViewModel searchResultViewModel) {
@@ -34,7 +43,24 @@ public class SearchResultView extends JPanel implements ActionListener, Property
         final JLabel title = new JLabel(SearchResultViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        movie = new JLabel();
+        movie = new JPanel();
+        movieTitle = new JLabel();
+        movieReleaseDate = new JLabel();
+        movieDescription = new JLabel();
+        movieRottenTomatoes = new JLabel();
+        movieGenre = new JLabel();
+        movieActors = new JLabel();
+        movieDirector = new JLabel();
+
+        movie.add(movieTitle);
+        movie.add(movieReleaseDate);
+        movie.add(movieDescription);
+        movie.add(movieRottenTomatoes);
+        movie.add(movieGenre);
+        movie.add(movieActors);
+        movie.add(movieDirector);
+
+        movie.setLayout(new BoxLayout(movie, BoxLayout.Y_AXIS));
 
         final JPanel buttons = new JPanel();
         toHome = new JButton(SearchResultViewModel.TO_HOME_BUTTON_LABEL);
@@ -58,7 +84,15 @@ public class SearchResultView extends JPanel implements ActionListener, Property
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equals("state")) {
             final SearchResultState state = (SearchResultState) evt.getNewValue();
-            movie.setText(state.getResult().toString());
+            final Movie movie = state.getResult();
+
+            setMovieTitle(movie);
+            setMovieReleaseDate(movie);
+            setMovieDescription(movie);
+            setMovieRottenTomatoes(movie);
+            setMovieGenre(movie);
+            setMovieActors(movie);
+            setMovieDirector(movie);
         }
     }
 
@@ -77,4 +111,63 @@ public class SearchResultView extends JPanel implements ActionListener, Property
     public void actionPerformed(ActionEvent evt) {
         System.out.println("Click " + evt.getActionCommand());
     }
+
+    public void setMovieTitle(Movie movie) {
+        movieTitle.setText("Title: " + movie.getTitle());
+    }
+
+    public void setMovieReleaseDate(Movie movie) {
+        if (movie.getReleaseDate().equals("N/A")) {
+            movieReleaseDate.setText("Release date not available.");
+        }
+        else {
+            movieReleaseDate.setText("Release Date: " + movie.getReleaseDate());
+        }
+    }
+
+    public void setMovieDescription(Movie movie) {
+        if (movie.getDescription().equals("N/A")) {
+            movieDescription.setText("Description not available.");
+        }
+        else {
+            movieDescription.setText("Description: " + movie.getDescription());
+        }
+    }
+
+    public void setMovieRottenTomatoes(Movie movie) {
+        if (movie.getRottenTomatoes() == -1) {
+            movieRottenTomatoes.setText("Rotten Tomatoes not available.");
+        }
+        else {
+            movieRottenTomatoes.setText("Rotten Tomatoes: " + String.valueOf(movie.getRottenTomatoes()));
+        }
+    }
+
+    public void setMovieGenre(Movie movie) {
+        if (movie.getGenre().isEmpty()) {
+            movieGenre.setText("Genres not available.");
+        }
+        else {
+            movieGenre.setText("Genres: " + movie.getGenre().toString());
+        }
+    }
+
+    public void setMovieActors(Movie movie) {
+        if (movie.getActors().isEmpty()) {
+            movieActors.setText("Actors not available.");
+        }
+        else {
+            movieActors.setText("Actors: " + movie.getActors().toString());
+        }
+    }
+
+    public void setMovieDirector(Movie movie) {
+        if (movie.getDirector().isEmpty()) {
+            movieDirector.setText("Directors not available.");
+        }
+        else {
+            movieDirector.setText("Directors: " + movie.getDirector().toString());
+        }
+    }
+
 }
