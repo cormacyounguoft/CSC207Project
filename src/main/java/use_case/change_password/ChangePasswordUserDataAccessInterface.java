85f425bf821c56bc67485a538204fe07cd7a65d0
Seko1112 <serdar.zhangali@mail.utoronto.ca>
2024-11-18 12:51:28 -0500

    ChangePassword use case modified to make it easier understand and effectively transfer all user data

--- src/main/java/use_case/change_password/ChangePasswordUserDataAccessInterface.java
+++ src/main/java/use_case/change_password/ChangePasswordUserDataAccessInterface.java
@@ -9,7 +9,7 @@ public interface ChangePasswordUserDataAccessInterface {
 
     /**
      * Updates the system to record this user's password.
-     * @param user the user whose password is to be updated
+     * @param password the user whose password is to be updated
      */
-    void changePassword(User user);
+    void changePassword(String username, String password);
 }
