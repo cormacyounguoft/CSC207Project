b56da6ef10efbe5feb838d256e47af7be826e145
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-21 18:33:07 -0500

    change pass interactor reverted

--- src/main/java/use_case/change_password/ChangePasswordInteractor.java
+++ src/main/java/use_case/change_password/ChangePasswordInteractor.java
@@ -23,17 +23,13 @@ public class ChangePasswordInteractor implements ChangePasswordInputBoundary {
 
     @Override
     public void execute(ChangePasswordInputData changePasswordInputData) {
-        if (!validatePassword(changePasswordInputData.getPassword())) {
-            userPresenter.prepareFailView("Invalid password. Ensure it is at least 8 characters long, "
-                    + "contains an uppercase letter, a lowercase letter, a digit, and a special character.");
-            return;
-        }
         final User user = userFactory.create(changePasswordInputData.getUsername(),
-                                             changePasswordInputData.getPassword());
-        userDataAccessObject.changePassword(user);
+                changePasswordInputData.getPassword());
+        userDataAccessObject.changePassword(changePasswordInputData.getUsername(),
+                changePasswordInputData.getPassword());
 
         final ChangePasswordOutputData changePasswordOutputData = new ChangePasswordOutputData(user.getName(),
-                                                                                  false);
+                false);
         userPresenter.prepareSuccessView(changePasswordOutputData);
     }
 
@@ -41,14 +37,5 @@ public class ChangePasswordInteractor implements ChangePasswordInputBoundary {
     public void switchToLoggedInView(ChangePasswordInputData inputData) {
         final ChangePasswordOutputData outputData = new ChangePasswordOutputData(inputData.getUsername(), false);
         userPresenter.switchToLoggedInView(outputData);
-    }
-
-    private boolean validatePassword(String password) {
-        return password != null &&
-                password.length() >= 8 &&
-                password.matches(".*[A-Z].*") && // Contains at least one uppercase letter
-                password.matches(".*[a-z].*") && // Contains at least one lowercase letter
-                password.matches(".*\\d.*") &&   // Contains at least one digit
-                password.matches(".*[(){}\\[\\]'\";:><,./?\\\\|\\+=\\-_\\*&^%$#@!~`].*"); // Contains at least one special character
     }
 }
