7703e354d59510a2afb4d6931680534eec6f87f8
cormacyounguoft <cormac.young@mail.utoronto.ca>
2024-11-25 14:08:26 -0500

    100% test coverage for LoggedInSearchResult

---
+++ src/test/java/use_case/logged_in_search_result/LoggedInSearchResultInteractorTest.java
@@ -0,0 +1,38 @@
+package use_case.logged_in_search_result;
+
+import interface_adapter.logged_in_search_result.LoggedInSearchResultPresenter;
+import org.junit.jupiter.api.Test;
+import use_case.logged_in_search.LoggedInSearchInputBoundary;
+import use_case.logged_in_search.LoggedInSearchInputData;
+import use_case.logged_in_search.LoggedInSearchInteractor;
+import use_case.logged_in_search.LoggedInSearchOutputBoundary;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class LoggedInSearchResultInteractorTest {
+    @Test
+    void successTest() {
+        LoggedInSearchResultInputData inputData = new LoggedInSearchResultInputData("username", "frozen");
+        LoggedInSearchResultOutputBoundary presenter = new LoggedInSearchResultOutputBoundary() {
+
+            @Override
+            public void prepareSuccessView(LoggedInSearchResultOutputData outputData) {
+                assertEquals("frozen", outputData.getMovie());
+                assertEquals("username", outputData.getUsername());
+                assertFalse(outputData.isUseCaseFailed());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case failure is unexpected.");
+            }
+        };
+
+        LoggedInSearchResultInputBoundary interactor = new LoggedInSearchResultInteractor(presenter);
+        interactor.execute(inputData);
+    }
+
+}
+
