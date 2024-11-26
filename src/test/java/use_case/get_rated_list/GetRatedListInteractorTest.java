5b1457643a85285985ea3b62fcbe223a35a00e80
cormacyounguoft <cormac.young@mail.utoronto.ca>
2024-11-26 15:25:35 -0500

    100% test coverage for get rated list

--- src/test/java/use_case/get_rated_list/GetRatedListInteractorTest.java
+++ src/test/java/use_case/get_rated_list/GetRatedListInteractorTest.java
@@ -1,48 +1,58 @@
 package use_case.get_rated_list;
 
-import data_access.InMemoryUserDataAccessObject;
-import data_access.MovieAccessObject;
-import entity.*;
+import entity.CommonUserFactory;
+import entity.Movie;
+import entity.MovieFactory;
+import entity.User;
+import entity.UserFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import use_case.search.*;
+import use_case.MockDataAccessObject;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GetRatedListInteractorTest {
+    MockDataAccessObject dataAccessObject;
+
+    @BeforeEach
+    void setUp() {
+        dataAccessObject = new MockDataAccessObject();
+        UserFactory factory = new CommonUserFactory();
+        User user = factory.create("Username", "Password123!");
+        dataAccessObject.save(user);
+
+        MovieFactory movieFactory = new MovieFactory();
+        Movie movie = movieFactory.create();
+        movie.setTitle("Movie");
+        movie.setPosterLink("url");
+        dataAccessObject.saveToWatchedList("Username", movie);
+        dataAccessObject.saveUserRating("Username", "Movie", 0);
+    }
+
+    @AfterEach
+    void tearDown() {
+        dataAccessObject.clear();
+    }
+
     @Test
     void successTest() {
-        InMemoryUserDataAccessObject userRepository = new InMemoryUserDataAccessObject();
-        UserFactory factory = new CommonUserFactory();
-        User user = factory.create("Paul", "Password1!");
-        Movie movie = new Movie();
-        userRepository.save(user);
-        movie.setTitle("Frozen");
-        movie.setPosterLink("https://m.media-amazon.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SX300.jpg");
-        userRepository.saveToWatchedList("Paul", movie);
-        userRepository.saveUserRating("Paul", "Frozen", 5);
-        GetRateListInputData inputData = new GetRateListInputData("Paul");
-
+        GetRateListInputData inputData = new GetRateListInputData("Username");
 
         GetRateListOutputBoundary presenter = new GetRateListOutputBoundary() {
             @Override
-            public void prepareSuccessView(GetRateListOutputData getRateListOutputData) {
-                assertEquals(getRateListOutputData.getUsername(), "Paul");
-                Map<String, List<String>> rating = new HashMap<>();
-                rating.put("Frozen", Arrays.asList("5", "https://m.media-amazon.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SX300.jpg"));
-                assertEquals(getRateListOutputData.getUserRating(), rating);
+            public void prepareSuccessView(GetRateListOutputData outputData) {
+                assertEquals("Username", outputData.getUsername());
+                assertTrue(outputData.getUserRating().containsKey("Movie"));
+                assertEquals(outputData.getUserRating().get("Movie").get(0), "0");
+                assertEquals(outputData.getUserRating().get("Movie").get(1), "url");
+                assertFalse(outputData.isUseCaseFailed());
             }
-        } ;
+        };
 
-
-        GetRateListInputBoundary interactor = new GetRateListInteractor(userRepository, presenter);
-
-
+        GetRateListInputBoundary interactor = new GetRateListInteractor(dataAccessObject, presenter);
         interactor.execute(inputData);
-
     }
 }
