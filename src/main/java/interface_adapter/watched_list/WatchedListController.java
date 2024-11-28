bc7b213e00cb115f7744cee28ea9e0a72fbc2381
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-28 03:17:18 -0500

    watched list checkstyle complete

--- src/main/java/interface_adapter/watched_list/WatchedListController.java
+++ src/main/java/interface_adapter/watched_list/WatchedListController.java
@@ -1,11 +1,11 @@
 package interface_adapter.watched_list;
 
-import entity.MovieList;
 import use_case.watched_list.WatchedListInputBoundary;
 import use_case.watched_list.WatchedListInputData;
 
-import java.util.List;
-
+/**
+ * The controller for the watched list Use Case.
+ */
 public class WatchedListController {
     private final WatchedListInputBoundary interactor;
 
@@ -13,6 +13,11 @@ public class WatchedListController {
         this.interactor = interactor;
     }
 
+    /**
+     * Execute the watched list use case.
+     * @param username username of the user.
+     * @param title title of the movie in the watched list.
+     */
     public void execute(String username, String title) {
         final WatchedListInputData inputData = new WatchedListInputData(username, title);
         interactor.execute(inputData);
