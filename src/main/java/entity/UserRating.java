c041f48541330340e98417c70a746d0615ebcdb8
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-27 20:39:57 -0500

    User rating checkstyle fixed

--- src/main/java/entity/UserRating.java
+++ src/main/java/entity/UserRating.java
@@ -13,23 +13,46 @@ public class UserRating {
         this.movieToRating = new HashMap<>();
     }
 
+    /**
+     * Add a rating to the User profile.
+     * @param movie The movie rated.
+     * @param rating The rating given.
+     */
     public void addRating(Movie movie, int rating) {
         this.movieToRating.put(movie.getTitle(), rating);
     }
 
+    /**
+     * Remove a rating from the User profile.
+     * @param movie The movie rated.
+     */
     public void removeRating(Movie movie) {
         this.movieToRating.remove(movie.getTitle());
     }
 
+    /**
+     * Get a rating.
+     * @param movie The movie rated.
+     * @return rating given.
+     */
     public int getRating(Movie movie) {
         return this.movieToRating.getOrDefault(movie.getTitle(), -1);
     }
 
+    /**
+     * Get the count of ratings.
+     * @return the rating count
+     */
     public int getRatingCount() {
         return this.movieToRating.size();
     }
 
-    public Map<String, Integer> getMovieToRating(){
+    /**
+     * Get the movie rating.
+     * @return the rating of the movie.
+     */
+    public Map<String, Integer> getMovieToRating() {
         return this.movieToRating;
     }
+
 }
