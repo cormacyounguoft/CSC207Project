f9ae44f7fa5e51464192ebe52003e637b2e40509
Seko1112 <serdar.zhangali@mail.utoronto.ca>
2024-11-27 23:29:23 -0500

    100% for ToLoggedInViewInteractor, deleted unnecessary boolean from ToLoggedInView use case outputdata

---
+++ src/test/java/use_case/to_logged_in_view/ToLoggedInViewInteractorTest.java
@@ -0,0 +1,20 @@
+package use_case.to_logged_in_view;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToLoggedInViewInteractorTest {
+
+    @Test
+    void successTest() {
+        ToLoggedInViewInputData inputData = new ToLoggedInViewInputData("Username");
+        ToLoggedInViewOutputBoundary presenter = new ToLoggedInViewOutputBoundary() {
+            @Override
+            public void prepareSuccessView(ToLoggedInViewOutputData outputData) {
+                assertEquals(outputData.getUsername(), "Username");
+            }
+        };
+        ToLoggedInViewInputBoundary interactor = new ToLoggedInViewInteractor(presenter);
+        interactor.toLoggedInView(inputData);
+    }
+}
