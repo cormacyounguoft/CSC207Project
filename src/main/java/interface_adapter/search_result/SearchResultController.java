36ef632c75c4c5c8516a904500af19f7513fbbbc
cormacyounguoft <cormac.young@mail.utoronto.ca>
2024-11-29 15:48:35 -0500

    Improved interface adapter for search result

--- src/main/java/interface_adapter/search_result/SearchResultController.java
+++ src/main/java/interface_adapter/search_result/SearchResultController.java
@@ -6,6 +6,7 @@ import use_case.search_result.SearchResultInputBoundary;
  * Controller for the Search Result Use Case.
  */
 public class SearchResultController {
+
     private final SearchResultInputBoundary searchResultUseCaseInteractor;
 
     public SearchResultController(SearchResultInputBoundary searchResultUseCaseInteractor) {
