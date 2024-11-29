8ad4f331121ccb798515f04098f979f6d0a2e708
Seko1112 <serdar.zhangali@mail.utoronto.ca>
2024-11-29 18:47:50 -0500

    Resolved magic number issues in `ChangePasswordView` by creating a separate file to store all constants.

---
+++ src/main/java/view/Constants.java
@@ -0,0 +1,29 @@
+package view;
+
+public class Constants {
+    public static final int BUTTON_WIDTH = 150;
+    public static final int BUTTON_HEIGHT = 50;
+    public static final int MAIN_BORDER_LAYOUT = 20;
+    public static final int LABEL_BORDER_LAYOUT = 5;
+    public static final int COLOUR_R = 240;
+    public static final int COLOUR_G = 248;
+    public static final int COLOUR_B = 255;
+    public static final int FONT = 24;
+    public static final int FONT1 = 16;
+    public static final int FONT2 = 12;
+    public static final int FONT3 = 22;
+    public static final int FONT_COLOUR_R = 0;
+    public static final int FONT_COLOUR_G = 51;
+    public static final int FONT_COLOUR_B = 102;
+    public static final String FONT_TYPE = "SansSerif";
+    public static final int LINE_BORDER_R = 173;
+    public static final int LINE_BORDER_G = 216;
+    public static final int LINE_BORDER_B = 230;
+    public static final int LINE_BORDER_THICKNESS = 1;
+    public static final int BACKGROUND_COLOUR_R = 93;
+    public static final int BACKGROUND_COLOUR_G = 186;
+    public static final int BACKGROUND_COLOUR_B = 255;
+    public static final int BORDER_COLOUR_R = 124;
+    public static final int BORDER_COLOUR_G = 183;
+    public static final int BORDER_COLOUR_B = 205;
+}
