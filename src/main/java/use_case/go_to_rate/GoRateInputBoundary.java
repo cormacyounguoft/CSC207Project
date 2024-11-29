bd9ae124a2d7691ccccc80c205c78f853a8571c1
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-28 22:26:32 -0500

    go to rate use case checkstyle done

--- src/main/java/use_case/go_to_rate/GoRateInputBoundary.java
+++ src/main/java/use_case/go_to_rate/GoRateInputBoundary.java
@@ -1,6 +1,14 @@
 package use_case.go_to_rate;
 
+/**
+ * Input boundary for go rate use case.
+ */
 public interface GoRateInputBoundary {
+
+    /**
+     * Switch to rate view.
+     * @param goRateInputData input data for go rate use case.
+     */
     void switchToRateView(GoRateInputData goRateInputData);
 
 }
--- src/main/java/use_case/go_to_rate/GoRateOutputBoundary.java
+++ src/main/java/use_case/go_to_rate/GoRateOutputBoundary.java
@@ -1,6 +1,14 @@
 package use_case.go_to_rate;
 
+/**
+ * Output boundary for go rate use case.
+ */
 public interface GoRateOutputBoundary {
+
+    /**
+     * Switch to rate view.
+     * @param rateOutputData output data for go rate use case.
+     */
     void switchToRateView(GoRateOutputData rateOutputData);
 
 }
