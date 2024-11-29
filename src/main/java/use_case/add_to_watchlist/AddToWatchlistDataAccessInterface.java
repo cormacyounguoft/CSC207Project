2f31f8e56393f3622eeda05c6f2258f54413cbf0
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-28 21:57:38 -0500

    add to watchlist checkstyle

--- src/main/java/use_case/add_to_watchlist/AddToWatchlistDataAccessInterface.java
+++ src/main/java/use_case/add_to_watchlist/AddToWatchlistDataAccessInterface.java
@@ -7,7 +7,16 @@ import entity.Movie;
  */
 public interface AddToWatchlistDataAccessInterface {
 
+    /**
+     * Gets the current username of the user adding to watchlist.
+     * @return the username.
+     */
     String getCurrentUsername();
 
+    /**
+     * Prepares the success view when add to watched list.
+     * @param username Username of the user.
+     * @param movie Movie being added to watchlist.
+     */
     void saveToWatchlist(String username, Movie movie);
 }
--- src/main/java/use_case/add_to_watchlist/AddToWatchlistOutputBoundary.java
+++ src/main/java/use_case/add_to_watchlist/AddToWatchlistOutputBoundary.java
@@ -4,7 +4,16 @@ package use_case.add_to_watchlist;
  * The output boundary for the Add to watchlist Use Case.
  */
 public interface AddToWatchlistOutputBoundary {
+
+    /**
+     * Prepares the success view when add to watched list.
+     * @param outputData output data when added to watch list.
+     */
     void prepareSuccessView(AddToWatchlistOutputData outputData);
 
+    /**
+     * Prepares the fail view when add to watch list.
+     * @param errorMessage error message displayed when fail view executed.
+     */
     void prepareFailView(String errorMessage);
 }
