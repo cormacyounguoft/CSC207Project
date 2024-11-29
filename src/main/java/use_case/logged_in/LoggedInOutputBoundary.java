d876e44212f1ca6226b10bf555ff9f817d60af2e
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-28 22:29:37 -0500

    logged in checkstyle completed

--- src/main/java/use_case/logged_in/LoggedInOutputBoundary.java
+++ src/main/java/use_case/logged_in/LoggedInOutputBoundary.java
@@ -6,11 +6,13 @@ package use_case.logged_in;
 public interface LoggedInOutputBoundary {
     /**
      * Switches to the Change Password View.
+     * @param loggedInOutputData Output data for logged in use case.
      */
     void switchToChangePasswordView(LoggedInOutputData loggedInOutputData);
 
     /**
      * Switches to the Logged In Search View.
+     * @param loggedInOutputData Output data for logged in use case.
      */
     void switchToLoggedInSearchView(LoggedInOutputData loggedInOutputData);
 }
