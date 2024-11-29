46bc4eb4821fc4d162e3e54a1b83dc4e644a4b0d
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-28 22:10:56 -0500

    get watched list checkstyle complete

--- src/main/java/use_case/get_watched_list/GetWatchedListInputBoundary.java
+++ src/main/java/use_case/get_watched_list/GetWatchedListInputBoundary.java
@@ -1,6 +1,13 @@
 package use_case.get_watched_list;
 
+/**
+ * Input boundary for get watched list.
+ */
 public interface GetWatchedListInputBoundary {
 
+    /**
+     * Execute the get watched list use case.
+     * @param inputData input data for get watched list.
+     */
     void execute(GetWatchedListInputData inputData);
 }
