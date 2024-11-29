205987d29049cf81b7ab22eb28748ec269c6384c
cormacyounguoft <cormac.young@mail.utoronto.ca>
2024-11-29 13:36:10 -0500

    Improved MovieAccessObject

--- src/main/java/data_access/MovieAccessObject.java
+++ src/main/java/data_access/MovieAccessObject.java
@@ -1,17 +1,17 @@
 package data_access;
 
-import entity.Movie;
-import entity.MovieFactory;
-import kong.unirest.Unirest;
-import kong.unirest.json.JSONObject;
-import use_case.SearchDataAccessInterface;
-
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import entity.Movie;
+import entity.MovieFactory;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
+import use_case.SearchDataAccessInterface;
 
 /**
  * DAO for searching for movies using the API.
@@ -28,8 +28,7 @@ public class MovieAccessObject implements SearchDataAccessInterface {
         final JSONObject response = getResponse(url);
 
         if (response.get("Response").equals("True")) {
-            final MovieFactory movieFactory = new MovieFactory();
-            final Movie movie = movieFactory.create();
+            final Movie movie = new MovieFactory().create();
 
             movie.setTitle(getTitle(response));
             movie.setReleaseDate(getReleaseDate(response));
@@ -62,28 +61,25 @@ public class MovieAccessObject implements SearchDataAccessInterface {
     }
 
     private String getReleaseDate(JSONObject response) {
-        final String releaseDate = response.get("Released").toString();
         String result = "";
-        if (!N_A.equals(releaseDate)) {
-            result = releaseDate;
+        if (!N_A.equals(response.get("Released").toString())) {
+            result = response.get("Released").toString();
         }
         return result;
     }
 
     private String getDescription(JSONObject response) {
-        final String releasePlot = response.get("Plot").toString();
         String result = "";
-        if (!N_A.equals(releasePlot)) {
-            result = releasePlot;
+        if (!N_A.equals(response.get("Plot").toString())) {
+            result = response.get("Plot").toString();
         }
         return result;
     }
 
     private List<String> getGenre(JSONObject response) {
-        final String genreString = response.get("Genre").toString();
         List<String> result = new ArrayList<>();
-        if (!N_A.equals(genreString)) {
-            result = new ArrayList<>(Arrays.asList(genreString.split(COMMA_SPACE)));
+        if (!N_A.equals(response.get("Genre").toString())) {
+            result = new ArrayList<>(Arrays.asList(response.get("Genre").toString().split(COMMA_SPACE)));
         }
         return result;
     }
@@ -94,52 +90,45 @@ public class MovieAccessObject implements SearchDataAccessInterface {
      * @return -1 if rating not available.
      */
     private int getRottenTomatoes(JSONObject response) {
-        final String rottenTomatoesString = response.get("Ratings").toString();
-        final List<String> ratings = Arrays.asList(rottenTomatoesString.split(COMMA));
-
+        final List<String> ratings = Arrays.asList(response.get("Ratings").toString().split(COMMA));
         int result = -1;
 
         if (ratings.contains("{\"Source\":\"Rotten Tomatoes\"")) {
             final String rating = ratings.get(ratings.indexOf("{\"Source\":\"Rotten Tomatoes\"") + 1)
                     .replaceAll("\\D", "");
-
             result = Integer.parseInt(rating);
         }
         return result;
     }
 
     private List<String> getActors(JSONObject response) {
-        final String actorsString = response.get("Actors").toString();
         List<String> result = new ArrayList<>();
-        if (!N_A.equals(actorsString)) {
-            result = new ArrayList<>(Arrays.asList(actorsString.split(COMMA_SPACE)));
+        if (!N_A.equals(response.get("Actors").toString())) {
+            result = new ArrayList<>(Arrays.asList(response.get("Actors").toString().split(COMMA_SPACE)));
         }
         return result;
     }
 
     private List<String> getDirector(JSONObject response) {
-        final String directorString = response.get("Director").toString();
         List<String> result = new ArrayList<>();
-        if (!N_A.equals(directorString)) {
-            result = new ArrayList<>(Arrays.asList(directorString.split(COMMA_SPACE)));
+        if (!N_A.equals(response.get("Director").toString())) {
+            result = new ArrayList<>(Arrays.asList(response.get("Director").toString().split(COMMA_SPACE)));
         }
         return result;
     }
 
     private int getRuntime(JSONObject response) {
-        final String runtimeString = response.get("Runtime").toString();
         int result = -1;
-        if (!N_A.equals(runtimeString)) {
-            result = Integer.parseInt(runtimeString.replaceAll("\\D", ""));
+        if (!N_A.equals(response.get("Runtime").toString())) {
+            result = Integer.parseInt(response.get("Runtime").toString().replaceAll("\\D", ""));
         }
         return result;
     }
 
     private String getPosterLink(JSONObject response) {
-        final String posterLinkString = response.get("Poster").toString();
         String result = "";
-        if (!N_A.equals(posterLinkString)) {
-            result = posterLinkString;
+        if (!N_A.equals(response.get("Poster").toString())) {
+            result = response.get("Poster").toString();
         }
         return result;
     }
