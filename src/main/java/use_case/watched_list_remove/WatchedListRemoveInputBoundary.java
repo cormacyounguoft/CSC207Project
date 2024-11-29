3bc9f951ec0e5ceff36989604eb166c94e0da6b4
Samir Hendawi <samirhendawi@Samirs-MacBook-Pro-2.local>
2024-11-28 21:40:46 -0500

    watched list remove checkstyle fixes

--- src/main/java/use_case/watched_list_remove/WatchedListRemoveInputBoundary.java
+++ src/main/java/use_case/watched_list_remove/WatchedListRemoveInputBoundary.java
@@ -1,6 +1,13 @@
 package use_case.watched_list_remove;
 
+/**
+ * Input boundary for watched list remove.
+ */
 public interface WatchedListRemoveInputBoundary {
 
+    /**
+     * Execute the input boundary for watch list remove.
+     * @param inputData input data for the watch list remove use caase.
+     */
     void execute(WatchedListRemoveInputData inputData);
 }
--- src/main/java/use_case/watchlist_remove/WatchlistRemoveInputBoundary.java
+++ src/main/java/use_case/watchlist_remove/WatchlistRemoveInputBoundary.java
@@ -6,7 +6,7 @@ package use_case.watchlist_remove;
 public interface WatchlistRemoveInputBoundary {
 
     /**
-     * Exeute the input boundary for watch list remove.
+     * Execute the input boundary for watch list remove.
      * @param inputData input data for the watch list remove use caase.
      */
     void execute(WatchlistRemoveInputData inputData);
