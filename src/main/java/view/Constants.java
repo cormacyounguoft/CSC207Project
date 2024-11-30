dd2f291f7d9622095f72c8087ebd877a0462c4a9
Seko1112 <serdar.zhangali@mail.utoronto.ca>
2024-11-30 00:27:47 -0500

    Created 4 more constants

--- src/main/java/view/Constants.java
+++ src/main/java/view/Constants.java
@@ -40,4 +40,8 @@ public class Constants {
     public static final int SCROLLER_HEIGHT = 500;
     public static final int IMAGE_WIDTH = 150;
     public static final int IMAGE_HEIGHT = 225;
+    public static final int GRID_OTHER_DETAILS_GAP = 5;
+    public static final int GRID_HIGHLIGHTED_DETAILS_HGAP = 10;
+    public static final int GRID_DETAILS_VGAP = 10;
+    public static final int GRID_CONTENT_HGAP = 20;
 }
